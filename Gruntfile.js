cc2d4cbb8e447e7ef34d8d22f20dc5e1e969f29e
provencal <vincent@tetraed.re>
2015-02-16 19:58:17 +0100

    add clean uglify and copy to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,15 +11,40 @@ module.exports = function(grunt) {
       }
     },
 
+    clean: ['dist/'] ,
+
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
-    }
+    },
+
+
+    uglify: {
+      dist: {
+        files: {
+          'dist/ga-cnil-cookies.min.js': ['src/ga-cnil-cookies.js']
+        }
+      }
+    },
+
+    copy: {
+          dist: {
+            files: [
+              {expand: true,  cwd: 'src/', src: ['**.js','**.css'], dest: 'dist/', filter: 'isFile'},
+            ],
+          },
+        },
+
+
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('dist',['clean','jshint','uglify:dist','copy:dist']);
 
 };
